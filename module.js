b6f0b1a425a92ed3e2fce5b4edaae069aed40542
julesfred <jules.tf4@yahoo.com>
2017-11-27 20:30:11 -0500

    intro paragraph

--- module.js
+++ module.js
@@ -5,7 +5,7 @@
             $routeProvider
             .when("/home", {
                 template:`
-           
+                <p class="intro">After years of hosting radio talk shows, creating cinimatic content, and serving delicious soft-served ice cream, it was time to fullfill my dream into a reality. Now here I am with my own web-portofolio! I would love to welcome you to my site where you will discover my passion for web development and design. Enjoy!</p>
                 `,
             })
             .when("/about", {
