37c532f7a26f419bbd7471e6548f2a3cf346d5eb
julesfred <jules.tf4@yahoo.com>
2017-12-14 20:24:00 -0500

    portrait

--- module.js
+++ module.js
@@ -13,8 +13,12 @@
             .when("/about", {
                 template: `
                 <h2 class="element-animation">About...</h2>
-                <p class="element-animation">I am a Front-end Developer student at Grand Circus, located in Detroit, Michigan.  
+                <div class="portraitWrap">
+                <p class="element-animation" id="paraSize">I am a Front-end Developer student at Grand Circus, located in Detroit, Michigan.  
                 On a day-to-day basis, I am improving my front-end development skills, researching all types of coding languages, and designing unique frameworks. People enjoy working with me because I am a team leader and I bring only positive vibes to the work environment. When I am not working I am either playing a great game of golf or I am out and about spending quality time with my friends and family.
+                </p>
+                <img src="images/me.jpg" alt="" class="portrait">
+                </div>
                 `
             })
             .when("/projects", {
@@ -27,5 +31,6 @@
                 
                 `
             })
+            .otherwise({ redirectTo: "/home" });
         });
 })();
